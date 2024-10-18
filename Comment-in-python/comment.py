f7ec7419656d88462d071d24753aeba88c277c7c
ElyasShamal <Elyas.shamal2@gmail.com>
2024-10-18 00:38:23 -0400

    add welcome and comment

---
+++ Comment-in-python/comment.py
@@ -0,0 +1,14 @@
+
+
+# The first thing we’re going to do is show how to tell a computer to ignore a part of a program.
+# Text written in a program but not run by the computer is called a comment
+# Python interprets anything after a # as a comment.
+
+
+
+
+# question to consider 
+
+# does python have multiline comment like other coding languages ?
+
+# Answer is : No Python has no built function for multiline comments, but we can use several lines beginning with hash # 
---
+++ Welcome/my_name.py
@@ -0,0 +1,4 @@
+
+# Change Elyas Shamal to your name. Run the code to see what it does!
+my_name = "Elyas Shamal"     
+print("Hello and Welcome", my_name + "!") 
