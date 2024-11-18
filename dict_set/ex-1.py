db1d090c1141033df209ea06791b5ee18db8ca34
ElyasShamal <Elyas.shamal2@gmail.com>
2024-11-17 21:21:18 -0500

    add pickle examples

---
+++ dict_set/ex-1.py
@@ -0,0 +1,22 @@
+
+
+import pickle
+
+
+phonebook = {'Elyas': '1222-444-55', 'jone': '222-222-222', 'danial':'444-222-344', 'Mohammad ELyas': '0000000000000000'}
+
+
+
+with open('phonebook.dat', 'wb') as output_file:
+    pickle.dump(phonebook, output_file)
+
+
+
+with open('phonebook.dat', 'rb') as data_file:
+    pb = pickle.load(data_file)
+
+
+print(pb)
+
+
+
